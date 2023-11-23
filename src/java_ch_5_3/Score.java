9273d86699617818c5307ef65213e80fc96d7021
oopoo123 <oopoo123@naver.com>
2023-11-23 14:44:19 +0900

    열거 추가

---
+++ src/java_ch_5_3/Score.java
@@ -0,0 +1,10 @@
+package java_ch_5_3;
+
+public enum Score {
+	
+	A,
+	B,
+	C,
+	F
+
+}
---
+++ src/java_ch_5_3/Week.java
@@ -0,0 +1,13 @@
+package java_ch_5_3;
+
+public enum Week {
+	
+	MONDAY,
+	TUESDAY,
+	THURSDAY,
+	WEDNESDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+
+}
